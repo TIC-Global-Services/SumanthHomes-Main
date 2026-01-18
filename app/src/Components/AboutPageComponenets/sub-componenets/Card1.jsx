6b391c028f7c8b27f93e44b31eb95a03a2e29143
santhosh-madeshMG <24j031@amjaincollege.edu.in>
2026-01-18 19:07:46 +0530

    replace the gsap animations with framer motion

--- app/src/Components/AboutPageComponenets/sub-componenets/Card1.jsx
+++ app/src/Components/AboutPageComponenets/sub-componenets/Card1.jsx
@@ -1,5 +1,6 @@
 import { useRef, useEffect } from "react"
 import { TextPlugin, gsap, ScrollTrigger } from "gsap/all"
+import { motion } from "motion/react";
 
 gsap.registerPlugin(TextPlugin);
 gsap.registerPlugin(ScrollTrigger);
@@ -9,43 +10,15 @@ export const Card1 = ({number, content, pageNo}) => {
   const numberRef = useRef(null);
   const cardRef = useRef(null);
 
-  useEffect(()=>{
-    const el = numberRef.current;
-
-    gsap.to(el, {
-      duration:1,
-      color:'#B40505',
-      text:{
-        value:number,
-      },
-      scrollTrigger:{
-        trigger:el,
-        start:"top 80%"
-      }
-    })
-
-    gsap.fromTo(cardRef.current, {
-          y:100,
-          scrollTrigger:{
-            trigger:cardRef.current,
-            toggleActions:"play none none reset",
-        }
-        },
-        {
-          y:0,
-          duration:.5,
-          scrollTrigger:{
-            trigger:cardRef.current,
-            toggleActions:"play none none reset"
-          }
-        })
-
-  },[])
-
   return (
 
 
-    <div ref={cardRef} className='
+    <motion.div
+     initial={{opacity:0, y:40}}
+     whileInView={{opacity:1, y:0}}
+     viewport={{once:false}}
+
+     ref={cardRef} className='
     
     flex items-center justify-between
     md:gap-5 border-t-2
@@ -55,12 +28,12 @@ export const Card1 = ({number, content, pageNo}) => {
 
         <div className='md:w-[60%] xl:w-[60%] '>
 
-            <p className='font-normal tracking-wide'> <span ref={numberRef} className='text-black'>{number}</span> {content}</p>
+            <p className='font-normal tracking-wide'> <span ref={numberRef} className='text-[#B40505]'>{number}</span> {content}</p>
 
         </div>
 
         <p className=' font-medium'>{pageNo}</p>
-    </div>
+    </motion.div>
 
   )
 }
