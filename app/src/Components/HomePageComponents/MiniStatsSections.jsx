91fd38bffc6fbb788d5ece9ff8c84a11189803c7
santhosh-madeshMG <24j031@amjaincollege.edu.in>
2026-01-20 22:52:26 +0530

    refactoring code

--- app/src/Components/HomePageComponents/MiniStatsSections.jsx
+++ app/src/Components/HomePageComponents/MiniStatsSections.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
 import { Card2 } from './sub-components/card2'
-import { TextSlideInAnimation } from '../../utils/TextSlideInAnimation'
-import { ScrollTrigger, gsap } from 'gsap/all'
-import { useEffect, useRef, useLayoutEffect } from 'react'
-
-gsap.registerPlugin(ScrollTrigger);
 
 export const MiniStatsSections = () => {
 
+  const data = [
+    {
+      number : "100",
+      plus : true,
+      title : "Homes purchased",
+      content : "Trusted by hundreds of happy homeowners ."
+    },
+    {
+      number : "20",
+      plus : false,
+      title : "Published properties",
+      content : "Sophisticated apartments with top amenities."
+    },
+    {
+      number : "13",
+      plus : false,
+      title : "Years of experience",
+      content : "Built on years of trust and timeless craftsmanship."
+    },
+  ]
 
   return (
 
@@ -16,9 +30,11 @@ export const MiniStatsSections = () => {
     gap-1
     my-16'>
 
-      <Card2 Number='100' plus={true} titleText='Homes purchased' contentText='Trusted by hundreds of happy homeowners .' />
-      <Card2 Number='20' titleText='Published properties' contentText='Sophisticated apartments with top amenities.' />
-      <Card2 Number='13' titleText='Years of experience' contentText='Built on years of trust and timeless craftsmanship.' />
+      {
+        data.map((data, id)=>(
+          <Card2 key={id} Number={data.number} plus={data.plus} titleText={data.title} contentText={data.content} />
+        ))
+      }
 
     </div>
   )
