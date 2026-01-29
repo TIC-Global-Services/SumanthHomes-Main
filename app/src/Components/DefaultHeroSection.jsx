f32b7c34f5c99702505173d6b5fcddcee86f2192
santhosh-madeshMG <24j031@amjaincollege.edu.in>
2026-01-29 10:33:28 +0530

    set every url's entry point to index.html

--- app/src/Components/DefaultHeroSection.jsx
+++ app/src/Components/DefaultHeroSection.jsx
@@ -13,7 +13,8 @@ export const DefaultHeroSection = ({bgImage, mainText, subText, white=false}) =>
     <div className='
     flex items-center justify-center
     w-[100%] 
-    h-[100dvh] md:h-[100dvh] xl:h-auto
+    h-[100dvh] md:h-[100dvh
+    ] xl:h-auto
     xl:aspect-[1440/553]
     ' style={{
 
