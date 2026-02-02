e35e6ef5bc39814997507a78ca83542602840f26
santhosh-madeshMG <24j031@amjaincollege.edu.in>
2026-02-02 12:06:03 +0530

    set get direction functionaliy

--- app/src/Components/HomePageComponents/MapSection.jsx
+++ app/src/Components/HomePageComponents/MapSection.jsx
@@ -1,11 +1,24 @@
 import { SplitText} from "gsap/SplitText"
 import gsap from "gsap/all"
+import { useState, useEffect } from "react";
 
 gsap.registerPlugin(SplitText);
 
 const MapSection = () => {
   
+  const [latitude, setLatitude] = useState('0');
+  const [longitude, setLongitude] = useState('0');
 
+  useEffect(()=>{
+
+    const setPosition = (position) =>{
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude)
+    }
+
+    navigator.geolocation.getCurrentPosition(setPosition)
+    
+  },[])
 
   return (
     <div className='mb-20 xl:mb-36 md:mb-14'>
@@ -44,13 +57,19 @@ const MapSection = () => {
             <div className='w-[10%]  border-t-0 md:border-t-2 border-black'>
 
             </div>
-            
+
             <button className='
             textAnimate
             text-[#B50404] font-semibold tracking-[-0.5px]
             w-[100%] md:w-auto
             text-[18px] md:text-xl
-            '>Get directions</button>
+            '>
+              <a className="w-full" href={`https://www.google.com/maps/dir/${latitude},${longitude}/Tirupati,+Andhra+Pradesh/@13.2822401,79.51811,10z/data=!3m1!4b1!4m18!1m8!3m7!1s0x3a4d4b0f88620427:0xcf4152d1daca0cac!2sTirupati,+Andhra+Pradesh!3b1!8m2!3d13.6287557!4d79.4191795!16zL20vMDg5bHJj!4m8!1m1!4e1!1m5!1m1!1s0x3a4d4b0f88620427:0xcf4152d1daca0cac!2m2!1d79.4191795!2d13.6287557?entry=ttu&g_ep=EgoyMDI2MDEyOC4wIKXMDSoKLDEwMDc5MjA2N0gBUAM%3D`} target="_blank">
+            Get directions
+            </a>
+              </button> 
+            
+            
         </div>
 
     </div>
