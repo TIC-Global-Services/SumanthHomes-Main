89ce7f91ecd45f97fa459732f0e4a0a094fa6eb4
santhosh-madeshMG <24j031@amjaincollege.edu.in>
2026-02-06 13:19:05 +0530

    set mobile responsive

--- app/src/Components/OurProjectsPageComponents/ExploreMoreSection.jsx
+++ app/src/Components/OurProjectsPageComponents/ExploreMoreSection.jsx
@@ -36,8 +36,8 @@ export const ExploreMoreSection = () => {
 
   return (
     <div ref={containerRef} className='
-    overflow-hidden
-    flex justify-between
+    md:overflow-hidden
+    flex justify-between gap-6 md:gap-0 flex-wrap md:flex-nowrap
     mb-20
     mt-8
     '>
@@ -88,18 +88,18 @@ export const ExploreMoreSection = () => {
        
         
         className='
-        
-        hidden xl:block
+        block
         relative
+        w-full md:w-auto
         xl:w-[45%] 
         '>
-            <div className='aspect-[308/371] overflow-hidden'>
+            <div className='aspect-[243/300] md:aspect-[308/371] w-[70%] md:w-auto flex ml-auto  overflow-hidden'>
                 <ParallaxComponent className={'w-full'}>
                     <img className="w-full scale-150" src={image2} />
                 </ParallaxComponent>
             </div>
 
-            <div className="w-[70%] aspect-[203.33/242.05] xl:top-44 xl:right-40 absolute overflow-hidden ">
+            <div className="w-[50%] md:w-[70%] aspect-[170/202] md:aspect-[203.33/242.05] absolute top-36 left-0 md:top-44 md:left-auto md:right-40   overflow-hidden ">
                 <ParallaxComponent className='w-full'>
                     <img className="w-full scale-150" src={image1} />
                 </ParallaxComponent>
